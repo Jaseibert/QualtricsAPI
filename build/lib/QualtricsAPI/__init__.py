dc2be4c071193233991991d3adfdd9746c1c87c8
Jeremy Seibert <Jaseibert5@gmail.com>
2019-05-23 11:34:02 -0500

    Delete __init__.py

--- build/lib/QualtricsAPI/__init__.py
+++
@@ -1,8 +0,0 @@
-# __init__.py
-from QualtricsAPI.Setup import Credentials
-from QualtricsAPI.Survey import Responses
-from QualtricsAPI.JSON import Parser
-from QualtricsAPI.Contacts import MailingList
-from QualtricsAPI.Contacts import XMDirectory
-
-__all__ = ["Setup", "JSON", "Contacts", "Survey"]

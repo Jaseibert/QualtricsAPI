e8e44a607f13dcbd3fc5298e8ad91a850fcb88ba
Jaseibert <Jaseibert5@gmail.com>
2019-09-23 14:04:28 -0500

    62nd Commit - Standardized the survey_id parameter in the responses module to survey

--- QualtricsAPI/Survey/responses.py
+++ QualtricsAPI/Survey/responses.py
@@ -11,14 +11,14 @@ class Responses(Credentials):
     def __init__(self):
         return
 
-    def setup_request(self, file_format='csv', survey_id=None):
+    def setup_request(self, file_format='csv', survey=None):
         ''' This method sets up the request and handles the setup of the request for the survey.'''
 
-        assert len(survey_id) == 18, 'Hey there! It looks like your survey ID is a the incorrect length. It needs to be 18 characters long. Please try again.'
-        assert survey_id[:3] == 'SV_', 'Hey there! It looks like your survey ID is incorrect. You can find the survey ID on the Qualtrics site under your account settings. Please try again.'
+        assert len(survey) == 18, 'Hey there! It looks like your survey ID is a the incorrect length. It needs to be 18 characters long. Please try again.'
+        assert survey[:3] == 'SV_', 'Hey there! It looks like your survey ID is incorrect. You can find the survey ID on the Qualtrics site under your account settings. Please try again.'
 
         headers, url = self.header_setup(content_type=True, xm=False, path='responseexports/')
-        payload = '{"format":"' + file_format + '","surveyId":"' + survey_id + '"}'
+        payload = '{"format":"' + file_format + '","surveyId":"' + survey + '"}'
         request = r.request("POST", url, data=payload, headers=headers)
         response = request.json()
         try:
@@ -27,11 +27,11 @@ class Responses(Credentials):
         except:
             print(f"ServerError:\nError Code: {response['meta']['error']['errorCode']}\nError Message: {response['meta']['error']['errorMessage']}")
 
-    def send_request(self, file_format='csv', survey_id=None):
+    def send_request(self, file_format='csv', survey=None):
         '''This method sends the request, and sets up the download request.'''
         try:
             file = None
-            progress_id, url, headers = self.setup_request(file_format=file_format, survey_id=survey_id)
+            progress_id, url, headers = self.setup_request(file_format=file_format, survey=survey)
             check_progress = 0
             progress_status = "in progress"
             while check_progress < 100 and progress_status is not "complete" and file is None:
@@ -49,10 +49,10 @@ class Responses(Credentials):
         '''This function accepts the file format, and the survey id, and returns the responses associated with that survey.
 
         :param survey: This is the id associated with a given survey.
-        :return: a Pandas DataFrame with survey responses
+        :return: a Pandas DataFrame
         '''
 
-        download_request = self.send_request(file_format='csv', survey_id=survey)
+        download_request = self.send_request(file_format='csv', survey=survey)
         with zipfile.ZipFile(io.BytesIO(download_request.content)) as survey_zip:
             for s in survey_zip.infolist():
                 df = pd.read_csv(survey_zip.open(s.filename))
@@ -66,7 +66,7 @@ class Responses(Credentials):
         :return: a Pandas DataFrame with the Surveys questions
         '''
 
-        df = self.get_responses(survey_id=survey)
+        df = self.get_responses(survey=survey)
         questions = pd.DataFrame(df[:1].T)
         questions.columns = ['Questions']
         return questions

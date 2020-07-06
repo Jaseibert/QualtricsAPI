b02fbcff49336db9653e5bacbce44b88200e4d0d
Seibert Industries <seibertindustries@MacBook-Pro.local>
2020-07-06 12:46:24 -0500

    70th Commit - Added Custom Exception - 500 Error

--- QualtricsAPI/Exceptions/exceptions.py
+++ QualtricsAPI/Exceptions/exceptions.py
@@ -1,27 +1,4 @@
-class Error(Exception):
-    '''Primary class for handling server-side and HTTP errors.'''
-    pass
-
-class MailingListIDError(Error):
-    '''This Exception handles errors associated with the Mailing List Ids.'''
+class Qualtrics500Error(Exception):
+    '''This Exception handles errors associated with the HTTP 500 (BRE_0.2) responses.'''
     def __init__(self, msg):
-        self.msg = msg
-
-    def __str__(self):
-        return repr(self.msg)
-
-class ContactIDError(Error):
-    '''This Exception handles errors associated with Contact Ids.'''
-    def __init__(self, msg):
-        self.msg = msg
-
-    def __str__(self):
-        return repr(self.msg)
-
-class ServerError(Error):
-    '''This Exception handles errors associated with the HTTP responses.'''
-    def __init__(self, msg):
-        self.msg = msg
-
-    def __str__(self):
-        return repr(self.msg)
+        super().__init__(msg)

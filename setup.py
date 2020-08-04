a21ce67fdc243deb8667d324841cdd42001d96b5
Seibert Industries <jaseibert5@gmail.com>
2020-08-04 16:52:21 -0500

    84th Commit - Testing XM

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='QualtricsAPI',
-    version='0.3.5',
+    version='0.3.6',
     author='Jeremy A. Seibert',
     author_email='jeremy@seibert.industries',
     description="QualtricsAPI is a lightweight Python library for the Qualtrics Web API. ",

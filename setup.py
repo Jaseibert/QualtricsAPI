51dde4aacf6b032b7d332cf398ebe24bc6a865a7
Jeremy Seibert <Jaseibert5@gmail.com>
2020-08-04 14:17:26 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='QualtricsAPI',
-    version='0.2.9',
+    version='0.3.0',
     author='Jeremy A. Seibert',
     author_email='jeremy@seibert.industries',
     description="QualtricsAPI is a lightweight Python library for the Qualtrics Web API. ",

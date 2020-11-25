e7781ac45c1490f54eedbcaa306b287a1b39003d
Seibert Industries <jaseibert5@gmail.com>
2020-11-25 15:07:18 -0600

    Added truthy conditional assertions for non-XM users.

--- QualtricsAPI/Setup/credentials.py
+++ QualtricsAPI/Setup/credentials.py
@@ -16,13 +16,14 @@ class Credentials(object):
         :type token: str
         :param data_center: Your Qualtrics data center
         :type data_center: str
-        :param directory_id: Your Qualtrics directory id
+        :param directory_id: Your Qualtrics directory id (XM Directory Users-Only)
         :type directory_id: str
         :return: Nothing explicitly, However you just create enviornment variables that will populate you HTTP Headers.
         '''
         assert len(token) == 40, 'Hey there! It looks like your api token is a the incorrect length. It needs to be 40 characters long. Please try again.'
-        assert len(directory_id) == 20, 'Hey there! It looks like your api directory ID is a the incorrect length. It needs to be 20 characters long. Please try again.'
-        assert directory_id[:5] == 'POOL_', 'Hey there! It looks like your directory ID is incorrect. You can find the directory ID on the Qualtrics site under your account settings. Please try again.'
+        if directory_id: 
+            assert len(directory_id) == 20, 'Hey there! It looks like your api directory ID is a the incorrect length. It needs to be 20 characters long. Please try again.'
+            assert directory_id[:5] == 'POOL_', 'Hey there! It looks like your directory ID is incorrect. You can find the directory ID on the Qualtrics site under your account settings. Please try again.'
 
         os.environ['token'] = token
         os.environ['data_center'] = data_center
@@ -38,6 +39,9 @@ class Credentials(object):
         :param content_type: use to return json response.
         :return: a HTML header and base url.
         '''
+        if xm:
+            assert os.environ['directory_id'], 'Hey there! This endpoint is only accessible for XM Directory Users . If you have access to the XM Directory, then be sure to include your directory_id when you use the qualtrics_api_credentials() method. '
+
         header = {"x-api-token": os.environ['token']}
         path = 'directories/{0}/'.format(os.environ['directory_id']) if xm else path
         base_url = f"https://{os.environ['data_center']}.qualtrics.com/API/v3/{path}"
